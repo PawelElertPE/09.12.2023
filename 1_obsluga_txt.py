00246a372bd36374338fb7a050a2f5f034dd9af4
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-12-09 11:09:11 +0100

    11:06

--- 1_obsluga_txt.py
+++ 1_obsluga_txt.py
@@ -2,13 +2,13 @@
 
 from functions import *
 
-with open ('u2.txt', 'r', encoding = 'utf8') as file1:
-    content = file1.read()
-print(type(content))
-print(len(content))
+content = read_file('u2.txt')
 
+content = clear_text(content)
 print(f'liczba slow: {no_of_words(content)}')
 print(f'liczba roznych slow: {no_of_unique_words(content)}')
+
+
 
 
 
